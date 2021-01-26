87bc5713b7b377844dfb75cad9dd03ed0ac6191c
vkrouza <krouza.v@gmail.com>
2021-01-26 20:26:55 +0100

    comments section

--- nette-blog/app/Presenters/PostPresenter.php
+++ nette-blog/app/Presenters/PostPresenter.php
@@ -4,6 +4,7 @@ namespace App\Presenters;
 
 use Nette;
 use Nette\Application\UI\Form;
+use Nette\Forms\Form as FormsForm;
 
 class PostPresenter extends Nette\Application\UI\Presenter
 {
@@ -22,5 +23,42 @@ class PostPresenter extends Nette\Application\UI\Presenter
         }
 
         $this->template->post = $post;
+        $this->template->comments = $post->related('comments')->order('created_at');
+    }
+
+
+    protected function createComponentCommentForm(): Form
+    {
+
+        $form = new Form;
+
+        $form->addtext('name', 'Your name:')
+            ->setRequired();
+
+        $form->addEmail('email', 'Email:');
+
+        $form->addTextArea('content', 'Comment:')
+            ->setRequired();
+
+        $form->addSubmit('send', 'Publish comment');
+
+        $form->onSuccess[] = [$this, 'commentFormSucceeded'];
+
+        return $form;
+    }
+
+    public function commentFormSucceeded(\stdClass $values): void
+    {
+        $postId = $this->getParameter('postId');
+
+        $this->database->table('comments')->insert([
+            'post_id' => $postId,
+            'name' => $values->name,
+            'email' => $values->email,
+            'content' => $values->content,
+        ]);
+
+        $this->flashMessage('Thank you for your comment', 'success');
+        $this->redirect('this');
     }
 }
